55f1260eddf2c0a97cbc4d08918a94d4ef0cda4c
frankzhang21 <644379641@qq.com>
2019-01-10 22:43:42 +1100

    New function

--- read_excel_easy.R
+++ read_excel_easy.R
@@ -1,1 +1,18 @@
 library(tidyverse)
+library(readxl)
+read_excel_easy <- function(path,sheet=1,all=FALSE,col_types=c("text")){
+  if(length(path)==1){
+    return(read_excel(path = path,sheet = sheet,col_types = col_types))
+  }else{
+    if(all==FALSE){
+      return(map_df(path,~read_excel(.x,sheet = sheet,col_types = col_types)))
+    }else{
+      return(map(path,~pmap(list(
+        path=.x,
+        sheet=c(1:sheet),
+        col_types=col_types),read_excel)
+      ))
+    }
+  }
+}
+  
